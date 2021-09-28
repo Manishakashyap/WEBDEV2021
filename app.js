b8dcbfa381cab02848fc92c38ac796af77649112
Manishakashyap <manisha82958@gmail.com>
2021-09-28 18:56:22 +0530

    Datatype in JS

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+// everything is an object (properties and methods)
+
+console.log("Manisha") // console is a object and log is its method
+console.warn("Warning") // console is a object and warn is its method
+console.error("Hello")  // console is a object and error is its method
+// type script is tightly scripted and js is not that typescript is converted in js to run in browser
+
+// premitive datatype in js non-premitive datatype
+
+// string
+console.log(typeof "Hello Word")
+console.log(typeof "H")
+
+// numbers
+console.log(typeof 78)
+console.log(typeof 78.564)
+console.log(typeof 785456457968465186768465464)
+console.log(typeof -6954)
+console.log(typeof -52.78)
