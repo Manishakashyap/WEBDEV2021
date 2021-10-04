d136fdf3c4e0b554a39ddc0722ab62ab30f94220
Manishakashyap <manisha82958@gmail.com>
2021-10-04 17:31:50 +0530

    to-do list elements working

--- app.js
+++ app.js
@@ -1,4 +1,41 @@
-var arr = [1,2,3,4,5,6,7,8]
+var button = document.querySelector('button')
+var input = document.querySelector('input')
+var List = document.querySelector('ul')
 
-arr.slice(3,1) //here 3 is index and 1 is no of element
-console.log(arr)
+
+
+var chores = []
+
+var deleteItems = (value) =>{
+      // console.log(value)
+      
+      // console.log(chores)
+      const index = chores.indexOf(value)
+      chores.splice(index,1)
+      console.log(index)
+}
+
+
+const callbackfunc = (event) => {
+   const inputValue = input.value
+
+   if (chores.includes(inputValue)){
+      console.log('already exists')
+   } else {
+
+      chores.push(inputValue)
+      const element = document.createElement('li')
+      const textNode = document.createTextNode(inputValue)
+      element.appendChild(textNode)
+     List.appendChild(element)
+
+     element.addEventListener('click',(e) => {deleteItems
+      (e.target.innerHTML)})
+
+     // element.addEventListener('click',() => {console.log('clicked')})
+   //   element.addEventListener('click',(e) => {console.log(e.target.innerHTML)})
+
+   }
+}
+
+button.addEventListener('click',callbackfunc)
