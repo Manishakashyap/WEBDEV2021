6c077a89468eef312eb4f0ab6101c9bec7d2ccbd
Manishakashyap <manisha82958@gmail.com>
2021-10-07 17:11:05 +0530

    fetch data

--- app.js
+++ app.js
@@ -45,11 +45,103 @@
 //    console.log(data.json()) //got error
 // },2000)
 
-let response = fetch('https://jsonplaceholder.typicode.com/users').then (res => res.json()).then(data => {
-    console.log(data) // .then always return a new promise
-})
+// let response = fetch('https://jsonplaceholder.typicode.com/users').then (res => res.json()).then(data => {
+//     console.log(data) // .then always return a new promise
+// })
 
-data.forEach(element => {
-   data.
+// data.forEach((element => {
+//    data.
    
-});
+// });
+
+// //pri singh
+// var promise1 = new Promise((Name) => {
+//     setTimeout(() => {
+//         console.log("hello world")
+//     }, 3000);
+// })
+
+
+
+// //anisha
+// let promise = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{resolve("anisha")},3000);
+// })
+
+
+
+// //Anisha's code of task
+
+//     ul = document.querySelector("ul");
+//     let response = fetch('https://jsonplaceholder.typicode.com/users').then(resp => resp.json()).then(data => {
+//         console.log(data);
+//         append_data = (element)=>{
+//             lielement = document.createElement("li");
+//             user_detail = element['name']+element['username']+element['email']
+//             textnode = document.createTextNode(user_detail);
+//             lielement.appendChild(textnode);
+//             ul.appendChild(lielement);
+//         }
+//         data.forEach(append_data);
+//     })
+
+
+//     //kartik code
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(res => res.json())
+//     .then(data => {
+//         data.forEach(user => {
+//             console.log(user)
+//         })
+
+//     })
+
+
+
+    // ///practice
+    // const table = document.querySelector('table')
+
+    // console.log(table)
+
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    // .then(res => res.json())
+    // .then(data =>{
+
+    //     data.forEach(user => {
+         
+    //     let newRow = document.createElement('tr')
+    //      let dataID = document.createElement('td')
+    //      let dataIDtextNode = document.createTextNode(user.id)
+         
+    //      dataID.appendChild(dataIDtextNode)
+    //      newRow.appendChild(dataID)
+
+    //     table.appendChild(newRow)
+
+    //     })
+    // })
+
+
+
+    const table = document.querySelector('table')
+
+    console.log(table)
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data =>{
+
+        data.forEach(user => {
+         
+        let newRow = document.createElement('tr')
+        let template = `
+        <td>ID</td>
+        <td>Name</td>
+        <td>Email</td>
+        <td>User Name</td>`
+
+        table.appendChild(newRow)
+
+        })
+    })
+
