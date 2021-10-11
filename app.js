47dbaf50b217d636ce77e177dcb731f347ddcde6
Manishakashyap <manisha82958@gmail.com>
2021-10-11 18:58:56 +0530

    Path module

--- app.js
+++ app.js
@@ -1,51 +1,31 @@
-//Module is the file of code/object
-//module.export.greet to export
-//to import in another file require
+// //.....................Node.js..................
+// const path = require('path')
+// // console.log(path)
+// // console.log(path.resolve('./app.js'))
+
+// console.log(path.relative('/',"./app.js"))
+// // utils ka path respect to temp temp util.js
+// console.log(path.resolve("./app.js"))
+// //if we use absolute path  that in our computer we cant use in another computer
+
+
+ // Users\Manisha\Desktop\WEBDEV2021\app.js=resolve
+ // C:\Users\Manisha\Desktop\WEBDEV2021\app.js=relative
+
+
+//................extention............
+// const path = require('path')
+// console.log(path.extname('./app.js'))
 
 
 
-// // const greet = () =>{
-// //     console.log('gid')
-// // }
-// // console.log("Iran")
+//................dirname...................
+const path = require('path')
+// console.log(path.dirname('./temp/utils.js'))
 
-// const button = document.querySelector('button')
-// const input = document.querySelector('input')
-// const div = document.querySelector('div')
+//.................__dirname.........
+// console.log(__dirname)
 
-// console.log('hii')
-
-// button.addEventListener('click', (req, res) => {
-//      localStorage.setItem('InputVaalue',input.value)
-//     div.innerHTML = input.value
-    
-//     //console.log(div.localStorage.getItem(InputValue))
-//     //wrong for me
-
-//      div.innerHTML = localStorage.getItem(inputvalue)
-
-// })
-
-
-//.......................localstorage....................
-
-//console.log(localStorage)
-
-//localStorage.setItem('InputValue',"something")
-//localStorage.clear()
-//console.log(localStorage.getItem("InputValue"))
-
-
-const button = document.querySelector('button')
-const input = document.querySelector('input')
-const div = document.querySelector('div')
-
-const arr = [1,23,45,45]
-const val = JSON.stringify(arr)
-
-console.log(arr)
-console.log(val)
-
-localStorage.setItem('val',JSON.stringify(arr))//give string
-
-console.log(JSON.parse(localStorage.getItem('val')))//convert string to arr
+//........path/join().............
+console.log(path.join(__dirname,'app.js')) //same as resolve
+//only diff is.....
